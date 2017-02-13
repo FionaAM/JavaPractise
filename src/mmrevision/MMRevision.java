38f401b45c5bf93c2f1dafa4bef968682fa01ed9
fbruce <b00241687@studentmail.uws.ac.uk>
2017-02-13 12:09:23 +0000

    First Commit

---
+++ src/mmrevision/MMRevision.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mmrevision;
+import java.io.*;
+/**
+ *
+ * @author Fiona
+ */
+public class MMRevision {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int [] numbers = {10,20,30,40,50};
+        for (int x : numbers)
+        {
+          System.out.print(x);
+          System.out.print(", ");
+        }
+    }
+    
+}
